e7ef20affef6db436fdd7a0f318dca7c2a890f79
kiwidancebad <morelessequal@gmail.com>
2020-09-30 16:47:44 +0300

    ADD SPACE

--- labs/lab_1/calculator/Main.java
+++ labs/lab_1/calculator/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     System.out.println("2 + 2 = " + calc.sum(2, 2));
     System.out.println("2 - 2 = " + calc.del(2, 2));
+    
     System.out.println("2 / 2 = " + calc.div(2, 2));
     System.out.println("2 * 2 = " + calc.mul(2, 2));
   }
